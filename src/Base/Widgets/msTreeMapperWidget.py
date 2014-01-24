e50855299e496bb4c4358ef9a16812d07ae7c8e9
Guillaume Reinisch <reinisch.gui@gmail.com>
2014-01-24 13:26:13 +0100

    Buttons 'add child' 'setId' 'set comment' 'sanity check' implemented; 'remove child' 'transfert child' 'deep copy' inserted but not enabled

--- src/Base/Widgets/msTreeMapperWidget.py
+++ src/Base/Widgets/msTreeMapperWidget.py
@@ -11,6 +11,7 @@ from TreeModels.TreeMapperModel import *
 import inspect
 from Dialogs import *
 from TreeModels.simpletreemodel import *
+from Dialogs.simpleDialogs import *
 
 class MethodsTreeModel(TreeModel):
     def __init__(self, data, parent=None):
@@ -41,8 +42,47 @@ class msTreeMapperWidget(msWidget):
     def __init__(self,data,mainwindows):
         super(msTreeMapperWidget, self).__init__(data,mainwindows)
         
-        label0 = QtGui.QLabel("Id:" + data.getId() )
+        label0 = QtGui.QLabel("Id : " + data.getId() )
         self.vbox.addWidget(label0)
+        
+        hbox = QtGui.QHBoxLayout()
+        setIdBttn = QtGui.QPushButton("Set id")
+        setIdBttn.clicked.connect(self.setId)
+        hbox.addWidget(setIdBttn)
+        
+        setComment = QtGui.QPushButton("Set comment")
+        setComment.clicked.connect(self.setComment)
+        hbox.addWidget(setComment)
+        hbox.addStretch(1)
+        self.vbox.addLayout(hbox)
+        
+        hbox = QtGui.QHBoxLayout()
+        sanityBttn = QtGui.QPushButton("Sanity check")
+        sanityBttn.clicked.connect(data.sanityCheck)
+        hbox.addWidget(sanityBttn)
+        hbox.addStretch(1)
+        self.vbox.addLayout(hbox)
+        
+        hbox = QtGui.QHBoxLayout()
+        addChildBttn = QtGui.QPushButton("Add child")
+        addChildBttn.clicked.connect(self.addChild)
+        hbox.addWidget(addChildBttn)
+        
+        rmChildBttn = QtGui.QPushButton("remove child")
+        hbox.addWidget(rmChildBttn)
+        rmChildBttn.setEnabled(0)
+        
+        transfChildBttn = QtGui.QPushButton("transfert child")
+        hbox.addWidget(transfChildBttn)
+        transfChildBttn.setEnabled(0)
+        
+        deepCopyBttn = QtGui.QPushButton("Deep copy")
+        deepCopyBttn.setEnabled(0)
+        hbox.addWidget(deepCopyBttn)
+        hbox.addStretch(1)
+        self.vbox.addLayout(hbox)
+        
+        
         """
         self.vbox.addWidget(label0)
         self.listMethods = QtGui.QTreeView()
@@ -50,11 +90,25 @@ class msTreeMapperWidget(msWidget):
         self.listMethods.setModel( MethodsTreeModel(self.data,self.listMethods ) ) 
         self.vbox.addWidget(self.listMethods)
 	"""
-        self.setMinimumSize(550,100)
-        self.setMaximumSize(550,100)
+        self.setMinimumSize(550,150)
+        self.setMaximumSize(550,150)
         #print label1.size().width()
         self.groupbox.setTitle("Tree mapper")
 
+    def addChild(self):
+        widget=msMethodCallDialog( self.data.addChild,self.mainwindows.rootObject,self.mainwindows)
+        widget.exec_()
+ 
+    def setId(self):
+        widget = queryStringDialog("set the object's id","id : ", self.data.getId(), self.data.setId )
+        widget.exec_()
+        self.mainwindows.reload()
+ 
+    def setComment(self):
+        widget = queryStringDialog("set the object's comment","comment : ", self.data.getComments(), self.data.setComments )
+        widget.exec_()
+        self.mainwindows.reload()
+        
     def execMethod(self):
         """ Execute a method of the object selected in the tree
         
