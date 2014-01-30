41cebbff3b4de5dbc2ab8e77d567cd9cb99bc4d9
Guillaume Reinisch <reinisch.gui@gmail.com>
2014-01-30 13:37:11 +0100

    small cleaning

--- src/Antioch/msMotionCEA.cpp
+++ src/Antioch/msMotionCEA.cpp
@@ -60,6 +60,126 @@ namespace impact {
         //-------------------------------------------------------------------------------------------------
         //-------------------------------------------------------------------------------------------------
         
+	void msMotionCEA::parseFromAscii( string filename , string nameTarget) {
+	      
+	    IMPACT_LOGIN();
+	    ifstream in(filename.c_str());
+	    Antioch::skip_comment_lines(in, '#');
+
+            std::string name;
+            double      h_form, val;
+	    bool        found = 0;
+            size_t      n_int;
+            std::vector<double> coeffs;
+		
+            while ( (in.good()) && (!found)) {
+		  
+	       in >> name >> n_int >> h_form; 
+	       coeffs.clear();
+	       for (unsigned int interval=0; interval<n_int; interval++) {
+	           for (unsigned int n=0; n<10; n++) in >> val, coeffs.push_back(val);
+	                  
+	           if(name == nameTarget){ addCoeffsFit(coeffs);found = 1; }
+	       }
+	    }
+	    IMPACT_LOGOUT();
+	}
+
+        //-------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------
+        
+	vector<double> msMotionCEA::getCoeffsOfFit(size_t i) {
+	      
+	    IMPACT_LOGIN();
+	    IMPACT_EXCEPT_IF([&](){return i>3;},"index out of bounds: 3 fits maximum");
+	    if(i==0) return CoeffsFit_200_1000;
+	    if(i==1) return CoeffsFit_1000_6000;
+	    if(i==2) return CoeffsFit_6000_20000;
+	    IMPACT_LOGOUT();
+	    return vector<double>();
+	};
+
+        //-------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------
+         
+	vector<double> msMotionCEA::getAllCoeffsOfFit() {
+	      
+	    std::vector<double> coefs = CoeffsFit_200_1000;
+	    coefs.insert(coefs.end(), CoeffsFit_1000_6000.begin(),  CoeffsFit_1000_6000.end());
+	    coefs.insert(coefs.end(), CoeffsFit_6000_20000.begin(), CoeffsFit_6000_20000.end());
+	    return coefs;
+	};
+
+        //-------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------
+         
+	void msMotionCEA::addCoeffsFit(vector<double>& fit) {
+	    
+	    IMPACT_LOGIN();
+	    int n=nFit;
+	    IMPACT_EXCEPT_IF([&](){return n>=3;}, "CEA fit support 3 fits maximum" );
+		
+	    if( nFit==0 ) CoeffsFit_200_1000   = fit;
+	    if( nFit==1 ) CoeffsFit_1000_6000  = fit;
+	    if( nFit==2 ) CoeffsFit_6000_20000 = fit;
+	    nFit++;
+	    IMPACT_LOGOUT();
+	}
+	  
+        //-------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------
+        
+        double msMotionCEA::H(double T)   { 
+	      
+	    IMPACT_LOGIN();
+	    IMPACT_TRY([&](){ update();});
+	    
+	    Antioch::TempCache<double> cache(T);
+	    const double* a = 0;
+	    IMPACT_TRY([&](){ a = Calculator->coefficients( Calculator->interval(cache.T) );});
+	    
+	    double  H_on_RT =  -a[0]/cache.T2 + a[1]*cache.lnT/cache.T + a[2] + a[3]*cache.T/2.0 + a[4]*cache.T2/3.0 + a[5]*cache.T3/4.0 + a[6]*cache.T4/5.0 + a[8]/cache.T;
+	    IMPACT_LOGOUT();
+	    return H_on_RT * T * R;
+	}
+	  
+        //-------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------
+        
+        double msMotionCEA::S(double T) { 
+	      
+	    IMPACT_LOGIN();
+	    IMPACT_TRY([&](){ update();});
+	    
+	    Antioch::TempCache<double> cache(T);
+	    const double* a = 0;
+	    IMPACT_TRY([&](){ a = Calculator->coefficients( Calculator->interval(cache.T) );});
+	    
+	    double  S_on_R    =  -a[0]/cache.T2/2.0 - a[1]/cache.T + a[2]*cache.lnT + a[3]*cache.T + a[4]*cache.T2/2.0 + a[5]*cache.T3/3.0 + a[6]*cache.T4/4.0 + a[9];
+            IMPACT_LOGOUT();
+	    return S_on_R * R;
+	}
+	  
+        //-------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------
+        
+        double msMotionCEA::Cv(double T) { 
+	      
+	    IMPACT_LOGIN();
+	    IMPACT_TRY([&](){ update();});
+	    
+	    Antioch::TempCache<double> cache(T);
+	    const double* a = 0;
+	    IMPACT_TRY([&](){ a = Calculator->coefficients( Calculator->interval(cache.T) );});
+	    
+	    double  Cp_on_R =  a[0]/cache.T2 + a[1]/cache.T + a[2] + a[3]*cache.T + a[4]*cache.T2 + a[5]*cache.T3 + a[6]*cache.T4;
+            IMPACT_LOGOUT();
+  	    return Cp_on_R * R - R;
+        }
+	  
+        //-------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------
+        	
         ostream& msMotionCEA::print(ostream& out) const  {
             
             msMotion::print(out);
--- src/Antioch/msMotionCEA.h
+++ src/Antioch/msMotionCEA.h
@@ -60,10 +60,11 @@ namespace impact {
             
             void update() {
                 
-                LOGGER_ENTER_FUNCTION_DBG("void msMotionCEA::update()",getFullId());
+                IMPACT_LOGIN();
                 msMotion::update();
-		updateCalculator();
-                LOGGER_EXIT_FUNCTION2("void msMotionCEA::update()");
+		Calculator = boost::shared_ptr<Antioch::CEACurveFit<double> >(
+		             new Antioch::CEACurveFit<double>(getAllCoeffsOfFit()));
+                IMPACT_LOGOUT();
             }
             
         public:
@@ -71,205 +72,84 @@ namespace impact {
 	    msMotionCEA(): msMotion(), nFit(0) {
                 
                 constructVar("msMotionCEA","MotionCEA","motion CEA");
+		R =  csts::R; 
             }
             
             static boost::shared_ptr<msMotionCEA> New(){
                 
-                LOGGER_ENTER_FUNCTION_DBG("static boost::shared_ptr<msMotionCEA> msMotionCEA::New()","");
+                IMPACT_LOGIN_STATIC();
                 boost::shared_ptr<msMotionCEA> T( new msMotionCEA() );
                 T->initialize();
                 T->setParameters( msMotionParams::New() );
                 T->update();
-                LOGGER_EXIT_FUNCTION2("static boost::shared_ptr<msMotionCEA> msMotionCEA::New()");
+                IMPACT_LOGOUT();
                 return T;
             }
             
             void initialize() {
 	      
-                LOGGER_ENTER_FUNCTION_DBG("void msMotionCEA::initialize()","");
-                
+                IMPACT_LOGIN();
                 msMotion::initialize();
-		
-		declareAttribute(CoeffsFit_200_1000,"CoeffsFit_200_1000");
-		declareAttribute(CoeffsFit_1000_6000,"CoeffsFit_1000_6000");
+		declareAttribute(CoeffsFit_200_1000  ,"CoeffsFit_200_1000");
+		declareAttribute(CoeffsFit_1000_6000 ,"CoeffsFit_1000_6000");
 		declareAttribute(CoeffsFit_6000_20000,"CoeffsFit_6000_20000");
-		
-                LOGGER_EXIT_FUNCTION2("void msMotionCEA::initialize()");
+		declarePhysicalVariable( msUnit("J.mol^-1.K^-1"), &R, "R");
+		IMPACT_LOGOUT();
             }
             
-	    void parseFromAscii( string filename , string nameTarget) {
-	      
-	        ifstream in(filename.c_str());
-	      
-	        Antioch::skip_comment_lines(in, '#');
-
-                std::string name;
-                unsigned int n_int;
-                std::vector<double> coeffs;
-                double h_form, val;
-		bool found = 0;
-		
-                while ( (in.good()) && (!found)) {
-		  
-		      in >> name;   // Species Name
-		      in >> n_int;  // Number of T intervals: [200-1000], [1000-6000], ([6000-20000])
-		      in >> h_form; // Formation Enthalpy @ 298.15 K
-
-		      coeffs.clear();
-		      for (unsigned int interval=0; interval<n_int; interval++) {
-			
-	                  for (unsigned int n=0; n<10; n++)  {
-		              in >> val, coeffs.push_back(val);
-	                  } 
-	                  if(name==nameTarget) addCoeffsFit(coeffs);
-	              }
-		      if(name==nameTarget) found = 1;
-		}
-	    }
+	    void parseFromAscii( string filename , string nameTarget);
 	    
-	    vector<double> getCoeffsOfFit(size_t i) {
-	      
-	        if(i==0) return CoeffsFit_200_1000;
-	        if(i==1) return CoeffsFit_1000_6000;
-		if(i==2) return CoeffsFit_6000_20000;
-		
-		msException e("index out of bounds: 3 fits maximum","vector<double> msMotionCEA::getCoeffsOfFit(size_t i)",
-			  getFullId());
-		          
-		IMPACT_THROW_EXCEPTION(e);
-		return vector<double>();
-	    };
+	    vector<double> getCoeffsOfFit(size_t i);
 	    
-	    vector<double> getAllCoeffsOfFit() {
-	      
-	        std::vector<double> coefs;
-		for( size_t i=0;i<CoeffsFit_200_1000.size();i++) 
-		  coefs.push_back(CoeffsFit_200_1000[i]);
-		for( size_t i=0;i<CoeffsFit_1000_6000.size();i++) 
-		  coefs.push_back(CoeffsFit_1000_6000[i]);
-		for( size_t i=0;i<CoeffsFit_6000_20000.size();i++) 
-		  coefs.push_back(CoeffsFit_6000_20000[i]);
-		
-		return coefs;
-	    };
+	    vector<double> getAllCoeffsOfFit(); 
+	    
+	    void addCoeffsFit(vector<double>& fit);
 	    
 	    //! @name msMotion  methods
             //@{
 	    
-            double Q(double T){
-	      
-                msException e("The computation of Q for a msMotionCEA need to be implemented",
-			  "vector<double> msMotionCEA::Q(double T)",
-			   getFullId());
-		          
-		IMPACT_THROW_EXCEPTION(e);
-		
-                return QfromDOS(T);
-            };
-            
+            double Q(double T){ IMPACT_THROW_NOT_IMPLEMENTED(); }            
 	    
-            void computeDOS() {
-	      
-	        msException e("The computation of the DOS for a msMotionCEA need to be implemented",
-			  "vector<double> msMotionCEA::computeDOS()",
-			   getFullId());
-		          
-		IMPACT_THROW_EXCEPTION(e);
-            };;
-            
+            void computeDOS() { IMPACT_THROW_NOT_IMPLEMENTED(); }         
             
             //! Density of states at energy E
-            virtual double DOS( double E ){
-                
-	        msException e("The computation of the DOS for a msMotionCEA need to be implemented",
-			  "vector<double> msMotionCEA::DOS( double E)",
-			   getFullId());
-		          
-		IMPACT_THROW_EXCEPTION(e);
-		
-            };
+            virtual double DOS( double E ){IMPACT_THROW_NOT_IMPLEMENTED(); }      
 
-            double H(double T)   { 
-	      
-	        updateCalculator();
-		Antioch::TempCache<double> cache(T);
-		const double* a = Calculator->coefficients( Calculator->interval(cache.T) );
-		double  H_on_RT    =  -a[0]/cache.T2 + a[1]*cache.lnT/cache.T + a[2] + a[3]*cache.T/2.0 + a[4]*cache.T2/3.0 + a[5]*cache.T3/4.0 + a[6]*cache.T4/5.0 + a[8]/cache.T;
- 
-		return H_on_RT * T * getUnits()->convert( "J.mol^-1.K^-1",  csts::R );
-	        };
+            double H(double T);
 	    
-            double S(double T) { 
-	      
-	        updateCalculator();
-	        Antioch::TempCache<double> cache(T);
-		const double* a = Calculator->coefficients( Calculator->interval(cache.T) );
-		double  S_on_R    =  -a[0]/cache.T2/2.0 - a[1]/cache.T + a[2]*cache.lnT + a[3]*cache.T + a[4]*cache.T2/2.0 + a[5]*cache.T3/3.0 + a[6]*cache.T4/4.0 + a[9];
- 
-		return S_on_R * getUnits()->convert( "J.mol^-1.K^-1",  csts::R );
-	        };
-	     
+            double S(double T);
             
-	    double Cv(double T) { 
-	      
-	        updateCalculator();
-	        Antioch::TempCache<double> cache(T);
-		const double* a = Calculator->coefficients( Calculator->interval(cache.T) );
-		double  Cp_on_R =  a[0]/cache.T2 + a[1]/cache.T + a[2] + a[3]*cache.T + a[4]*cache.T2 + a[5]*cache.T3 + a[6]*cache.T4;
-		double R = getUnits()->convert( "J.mol^-1.K^-1",  csts::R );
-		return Cp_on_R * R - R;
-	        };  		
+	    double Cv(double T);  		
             
 	    //@}
 	    
             std::ostream& print(std::ostream& out) const ;
-	   
-	    void addCoeffsFit(vector<double>& fit) {
-	    
-	        if( nFit==0 ) CoeffsFit_200_1000 = fit;
-	        if( nFit==1 ) CoeffsFit_1000_6000 = fit;
-	        if( nFit==2 ) CoeffsFit_6000_20000 = fit;
-	        if( nFit>=3 ) {
-	      
-	            msException e("CEA fit support 3 fits maximum",
-			      "add msMotionCEA:: addCoeffsFit(vector<double>& fit) ",
-		  	      getFullId());
-		    IMPACT_THROW_EXCEPTION(e);
-	        }
-		nFit++;
-	  }
 	  
 	protected:
 	   	  
 	  vector<double> getFit(double T) {
 	    
-	      if( (T<200) || (T>20000) ) {
-	      
-	          msException e("CEA fit only for T>200 and T<20000 (T="+output::getString<double>(T)+")",
-			    "vector<double>& msMotionCEA:: getFit(double T) ",
-			    getFullId());
-		  IMPACT_THROW_EXCEPTION(e);
-	      }
+	      IMPACT_LOGIN();
+	      IMPACT_EXCEPT_IF( [&](){return (T<200) || (T>20000);}, "CEA fit only for T>200 and T<20000" );
+		
 	      if( T>=200  && T < 1000 )    return CoeffsFit_200_1000;
 	      if( T>=1000 && T < 6000 )    return CoeffsFit_1000_6000;
 	      if( T>=6000 && T <= 20000 )  return CoeffsFit_6000_20000;
+	      IMPACT_LOGOUT();
 	  }
 	  	    
         private:
             
+	  //! @name Attributs & children
+          //@{
 	  vector<double> CoeffsFit_200_1000;
 	  vector<double> CoeffsFit_1000_6000;
 	  vector<double> CoeffsFit_6000_20000;
-	  
+	  //@}
 	  boost::shared_ptr<Antioch::CEACurveFit<double> > Calculator;
 	  
-	  void updateCalculator() {
-	    Calculator = boost::shared_ptr<Antioch::CEACurveFit<double> >(
-		             new Antioch::CEACurveFit<double>(getAllCoeffsOfFit())
-									      );
-	  }
 	  size_t nFit;
-	  
+	  double R; //! physical variable, always in the this->getUnits() unit
 	};
         
     }
